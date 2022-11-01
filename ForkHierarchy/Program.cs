e5fae4c8ad2afe2bbc5a7fb5f6fa8b526349dd95
AlmightyLks <almightylks@protonmail.com>
2022-11-01 15:50:38 +0100

    Converting it into an app service

---
+++ ForkHierarchy/Program.cs
@@ -0,0 +1,38 @@
+using ForkHierarchy.Services;
+using Octokit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<GitHubClient>(x =>
+{
+    string? pat = x.GetService<IConfiguration>()?.GetValue<string>("Secrets:GitHubPAT");
+
+    var client = new GitHubClient(new ProductHeaderValue("AlmightyLks"));
+    client.Credentials = new Credentials(pat);
+    return client;
+});
+builder.Services.AddScoped<HierachyBuilder>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ ForkHierarchy/Services/HierachyBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Octokit;
+
+namespace ForkHierarchy.Services;
+
+public class HierachyBuilder
+{
+    private readonly GitHubClient _gitHubClient;
+
+    public HierachyBuilder(GitHubClient githubClient)
+    {
+        _gitHubClient = githubClient;
+    }
+
+    public async Task Build(string owner, string name)
+    {
+        var repository = await _gitHubClient.Repository.Get(owner, name);
+        //_gitHubClient.Repository.
+    }
+}
